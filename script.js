e5720332af5c5ef44cb5ceabcb72371fc9ddee7e
Jayant Manchanda <maanjayant@gmail.com>
2020-10-03 15:34:20 +0000

    Anagram checker implimented

--- script.js
+++ script.js
@@ -1,49 +1,31 @@
+function getCharMap(string) {
+  let map = {};
 
-
-
-
-
-function makeAnagram(string1, string2) {
-  let obj1 = {};
-  let obj2 = {};
-  let count = 0;
-  for (let char of string1) {
-    if (obj1[char]) {
-      obj1[char] += 1;
-    } else {
-      obj1[char] = 1;
-    }
+  for (let char of string) {
+    map[char] ? (map[char] += 1) : (map[char] = 1);
   }
-  //console.log(obj1);
-
-  for (let character of string2) {
-    if (obj2[character]) {
-      obj2[character] += 1;
-    } else {
-      obj2[character] = 1;
-    }
-  }
-
-  console.log(obj1, obj2);
-
-  for (let val in obj1) {
-    for (let value in obj2) {
-      if (val == value) {
-        count++;
-        console.log(value);
-      }
-    }
-  }
-  console.log(count++);
-  console.log(string1.length);
-  console.log(string1.length - count, string2.length - count, count);
-  let result = string1.length - count + (string2.length - count);
-  return result;
+  return map;
 }
 
-console.log(
-  makeAnagram(
-    "absdjkvuahdakejfnfauhdsaavasdlkj",
-    "djfladfhiawasdkjvalskufhafablsdkashlahdfa"
-  )
-);
+function checkAnagram(string1, string2) {
+  let count = 0;
+
+  if (string1.length !== string2.length) {
+    return "This cannot be an Anagram";
+  }
+
+  const charMap1 = getCharMap(string1);
+  const charMap2 = getCharMap(string2);
+  console.log(charMap1, charMap2, count);
+  for (let char of Object.keys(charMap1)) {
+    console.log(
+      charMap1[char] !== charMap2[char],
+      charMap1[char],
+      charMap2[char]
+    );
+    if (charMap1[char] !== charMap2[char]) {
+      return false;
+    }
+  }
+  return true;
+}
